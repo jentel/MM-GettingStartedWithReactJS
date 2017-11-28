373eb7faffe5fdc142876855ece6c2a5be7f6b72
MAGENIC\JenniferT <JenniferT@magenic.com>
2017-11-28 10:16:54 -0600

    -code clean up

--- reactjs/src/index.js
+++ reactjs/src/index.js
@@ -1,28 +1,17 @@
-// 1.) create a store
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
 import {combineReducers, createStore} from 'redux';
 
-// const reducer = (state, action) => {
-//     switch(action.type){
-//         case "INCREMENT":
-//             return state += action.incrementValue;
-//             break;
-//         case "DECREMENT":
-//             return state -= action.incrementValue;
-//             break;
-//     }
-//     return state;
-// }
-
 const userInformationReducer = (state={}, action) => {
-    // state === userInformation
     switch(action.type){
         case "UPDATE_USER_NAME":
-            // state.name = action.payload;
             return { ... state, name: action.payload}            
             break;
         case "UPDATE_USER_AGE":
-            //state.age = action.payload;
             return { ... state, age: action.payload}
             break;
         default:
@@ -32,7 +21,6 @@ const userInformationReducer = (state={}, action) => {
 }
 
 const preferredGreetingReducer = (state="", action) => {
-    // state === preferredGreeting
     switch(action.type)
     {
         case "UPDATE_PREFERRED_GREETING":
@@ -48,18 +36,6 @@ const reducers = combineReducers({
     userInformation: userInformationReducer,
     preferredGreeting: preferredGreetingReducer
 })
-
-/*
-    Actual JSON
-    {
-        userInformation: {
-            ..
-        }
-        password:
-        FetchingCall:
-        ...
-    }
-*/
 
 const applicationStore = createStore(reducers, {
     userInformation: {
@@ -81,11 +57,7 @@ applicationStore.dispatch({type:"UPDATE_USER_NAME", payload:"Art"});
 applicationStore.dispatch({type:"UPDATE_USER_AGE", payload:14});
 applicationStore.dispatch({type:"UPDATE_PREFERRED_GREETING", payload:"Oh Geez!"});
 
-// this is bad form but we'll keep it for now
-// applicationStore.dispatch({type:"INCREMENT", incrementValue:64});
-// applicationStore.dispatch({type:"INCREMENT", incrementValue:32});
-// applicationStore.dispatch({type:"INCREMENT", incrementValue:1});
-// applicationStore.dispatch({type:"INCREMENT", incrementValue:52});
-// applicationStore.dispatch({type:"DECREMENT", incrementValue:1000});
 
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
 
